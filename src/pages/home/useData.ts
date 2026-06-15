be93fb36363dde6242dc379f86f12b9c2daf8180
mohammad <aahrari5@gmail.com>
2026-06-15 14:39:42 +0330

    update bmi logic for build

--- src/pages/home/useData.ts
+++ src/pages/home/useData.ts
@@ -5,7 +5,7 @@ import { calculateBmiFormSchema, CalculateBmiFormValues } from '@schemas'
 // enums
 import { GenderEnum, HeightEnum, WeightEnum } from '@enums'
 // utils
-import { convertHeightToCentimeter, convertWeightToKilogram } from '@utils/converter'
+// import { convertHeightToCentimeter, convertWeightToKilogram } from '@utils/converter'
 
 export type Units = Pick<CalculateBmiFormValues, 'weightUnit' | 'heightUnit'>
 
@@ -29,12 +29,11 @@ export const useData = () => {
   /* -------------------------------- Handlers -------------------------------- */
 
   const handleSubmitFinish = () => {
-    const weight = convertWeightToKilogram(parseFloat(watch('weight')), watch('weightUnit'))
-    const height = convertHeightToCentimeter(parseFloat(watch('height')), watch('heightUnit')) / 100
-    const bmi = weight / (height * height)
-
-    const genderFactor = watch('gender') === GenderEnum.MALE ? 1 : 0
-    const bodyFat = 1.2 * bmi + 0.23 * parseFloat(watch('age')) - 10.8 * genderFactor - 5.4
+    // const weight = convertWeightToKilogram(parseFloat(watch('weight')), watch('weightUnit'))
+    // const height = convertHeightToCentimeter(parseFloat(watch('height')), watch('heightUnit')) / 100
+    // const bmi = weight / (height * height)
+    // const genderFactor = watch('gender') === GenderEnum.MALE ? 1 : 0
+    // const bodyFat = 1.2 * bmi + 0.23 * parseFloat(watch('age')) - 10.8 * genderFactor - 5.4
   }
 
   return {
